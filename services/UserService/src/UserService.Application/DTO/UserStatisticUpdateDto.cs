94175b385235fc5c47a914fe95c69951b3dbe8fe
Alexander <121170207+Hissul@users.noreply.github.com>
2025-06-30 19:41:45 +0300

    feat(user): Правка выявленных багов #user-2

--- services/UserService/src/UserService.Application/DTO/UserStatisticUpdateDto.cs
+++ services/UserService/src/UserService.Application/DTO/UserStatisticUpdateDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UserService.Application.DTO;
+﻿namespace UserService.Application.DTO;
 
 public class UserStatisticUpdateDto {
     public Guid UserId { get; set; }
