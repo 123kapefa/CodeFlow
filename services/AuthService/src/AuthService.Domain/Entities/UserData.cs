9a56ae207f1d58ebd9122ea31bdb319ff7e77311
artur <105075817+123kapefa@users.noreply.github.com>
2025-07-02 17:33:32 +0300

    feat(auth): Добавлена бизнес логика регистрации пользователя. Так же добавлена возможность создавать пользователя с помощью статического метода. #auth-5

--- services/AuthService/src/AuthService.Domain/Entities/UserData.cs
+++ services/AuthService/src/AuthService.Domain/Entities/UserData.cs
@@ -6,4 +6,11 @@ public class UserData : IdentityUser<Guid> {
 
   public bool IsExternal { get; set; }
 
+  protected UserData() {}
+
+  private UserData (string email) {}
+  
+  public static UserData Create (string email) {
+    return new UserData(email);
+  }
 }
