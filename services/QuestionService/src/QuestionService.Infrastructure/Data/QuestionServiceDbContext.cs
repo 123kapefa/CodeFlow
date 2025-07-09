2f9873b9558111bdfa0e32e197541d5e094201ba
Alexander <121170207+Hissul@users.noreply.github.com>
2025-07-09 18:27:11 +0300

    feat(question): Добавление и настройка QuestionServiceDbContext. Первая миграция. (#19)

---
+++ services/QuestionService/src/QuestionService.Infrastructure/Data/QuestionServiceDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuestionService.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionService.Infrastructure.Data;
+
+public class QuestionServiceDbContext : DbContext {
+
+    public QuestionServiceDbContext( DbContextOptions<QuestionServiceDbContext> options ) : base(options) { }
+
+    public DbSet<Question> Questions { get; set; }
+    public DbSet<QuestionChangingHistory> QuestionChangingHistories { get; set; }
+    public DbSet<QuestionTag> QuestionTags { get; set; }
+
+    protected override void OnModelCreating( ModelBuilder modelBuilder ){
+        modelBuilder.Entity<Question>()
+            .HasMany(q => q.QuestionChangingHistories)
+            .WithOne(h => h.Question)
+            .HasForeignKey(h => h.QuestionId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Question>()
+            .HasMany(q => q.QuestionTags)
+            .WithOne(qt => qt.Question)
+            .HasForeignKey(qt => qt.QuestionId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+
+}
