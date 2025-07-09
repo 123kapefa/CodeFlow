8b3df921a688be05d8d2d531fd2d964bce69dce1
artur <105075817+123kapefa@users.noreply.github.com>
2025-07-09 19:15:51 +0300

    feat(auth): Добавлены классы для настройки google и github аутентификации #auth-5

--- services/AuthService/src/AuthService.Infrastructure/Settings/JwtSettings.cs
+++ services/AuthService/src/AuthService.Infrastructure/Settings/JwtSettings.cs
@@ -1,10 +1,8 @@
-namespace AuthService.Infrastructure.Security;
+namespace AuthService.Infrastructure.Settings;
 
-public class JwtSettings
-{
+public class JwtSettings {
   public string Secret { get; set; }
   public string Issuer { get; set; }
   public string Audience { get; set; }
   public int ExpiresInMinutes { get; set; }
-  
 }
