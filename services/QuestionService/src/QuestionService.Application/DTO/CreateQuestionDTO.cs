093061f1b3a2208132bed042c1362a416dae3f68
Alexander <121170207+Hissul@users.noreply.github.com>
2025-07-12 14:00:59 +0300

    feat(question): Реализация Features и ApiEndpoins #question-5

--- services/QuestionService/src/QuestionService.Application/DTO/CreateQuestionDTO.cs
+++ services/QuestionService/src/QuestionService.Application/DTO/CreateQuestionDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuestionService.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +7,10 @@ using System.Threading.Tasks;
 
 namespace QuestionService.Application.DTO;
 
-public class CreateQuestionDTO {
+public class CreateQuestionDTO {  
+    public Guid UserId { get; set; } 
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow; 
+    public List<QuestionTag> QuestionTags { get; set; } = [];
 }
