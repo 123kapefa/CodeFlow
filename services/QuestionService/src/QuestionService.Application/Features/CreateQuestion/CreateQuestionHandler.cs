fb08539359793b7dd7a5a8827bcf77cdc714bf84
Alexander <121170207+Hissul@users.noreply.github.com>
2025-07-12 14:00:59 +0300

    feat(question): Реализация Features и ApiEndpoins #question-5

---
+++ services/QuestionService/src/QuestionService.Application/Features/CreateQuestion/CreateQuestionHandler.cs
@@ -0,0 +1,38 @@
+﻿using Ardalis.Result;
+using Contracts.Commands;
+using FluentValidation;
+using QuestionService.Domain.Entities;
+using QuestionService.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionService.Application.Features.CreateQuestion;
+
+public class CreateQuestionHandler : ICommandHandler<CreateQuestionCommand> {
+
+    private readonly IQuestionServiceRepository _questionServiceRepository;
+    private readonly IValidator<CreateQuestionCommand> _validator;
+
+    public CreateQuestionHandler( IQuestionServiceRepository questionServiceRepository, IValidator<CreateQuestionCommand> validator ) {
+        _questionServiceRepository = questionServiceRepository;
+        _validator = validator;
+    }
+
+    public async Task<Result> Handle( CreateQuestionCommand command, CancellationToken cancellationToken ) {
+
+        var validationResult = await _validator.ValidateAsync(command);
+
+        Question question = new Question {
+            UserId = command.QuestionDTO.UserId,
+            Title = command.QuestionDTO.Title,
+            Content = command.QuestionDTO.Content,
+            CreatedAt = DateTime.UtcNow,
+            QuestionTags = command.QuestionDTO.QuestionTags
+        };
+
+    }
+
+}
---
+++ services/QuestionService/src/QuestionService.Application/Features/CreateQuestion/CreateQuestionValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionService.Application.Features.CreateQuestion;
+
+public class CreateQuestionCommand : AbstractValidator<CreateQuestionCommand> {
+
+    public CreateQuestionCommand() {
+        RuleFor(q => q.QuestionDTO.UserId)
+            .NotEmpty().WithMessage("Id автора вопроса не может быть пустым");
+
+        RuleFor(q => q.QuestionDTO.Title)
+            .NotEmpty().WithMessage("Заголовок вопроса не может быть пустым");
+
+        RuleFor(q => q.QuestionDTO.Content)
+            .NotEmpty().WithMessage("Тело запроса не может быть пустым");
+
+        RuleFor(q => q.QuestionDTO.QuestionTags)
+            .Cascade(CascadeMode.Stop)
+            .NotNull().WithMessage("Список тэгов обязателен")
+            .Empty().WithMessage("Нужно указать минимум один тег");
+    }
+
+}
