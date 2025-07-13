7a9774f5df1b4055e698257284a9739e358f529b
artur <105075817+123kapefa@users.noreply.github.com>
2025-07-13 21:30:07 +0300

    features/auth/auth-5-business-logic (#20)
    * feat(auth): Добавлены классы для настройки google и github аутентификации #auth-5
    
    * feat(auth): очистка appsettings от секретов
    
    * fix(auth): Доработка возможности смены пароля, с возможностью получения письма подтверждения на почту #auth-5

--- services/AuthService/src/AuthService.Api/Program.cs
+++ services/AuthService/src/AuthService.Api/Program.cs
@@ -1,35 +1,77 @@
+using System.Security.Claims;
+
+using AuthService.Api.Extensions;
+using AuthService.Application.Abstractions;
+using AuthService.Application.Features.LoginUser;
+using AuthService.Application.Features.RegisterUser;
+using AuthService.Domain.Entities;
+using AuthService.Domain.Repositories;
 using AuthService.Infrastructure;
+using AuthService.Infrastructure.Email;
+using AuthService.Infrastructure.Security;
+using AuthService.Infrastructure.Settings;
+
+using FluentValidation;
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.OpenApi.Models;
 
 
 var builder = WebApplication.CreateBuilder (args);
 
-builder.Services.AddControllers ();
+var config = new ConfigurationBuilder()
+ .AddUserSecrets<Program>()
+ .Build();
 
-builder.Services.AddOpenApi ();
-builder.Services.AddScoped<AuthServiceDbContext> (_ =>
-  new AuthServiceDbContext (builder.Configuration.GetConnectionString (nameof(AuthServiceDbContext))!));
+builder.UseBase ();
+builder.UseDatabase ();
 
+builder.Services.AddScoped<IValidator<RegisterUserCommand>, RegisterUserValidator> ();
+builder.Services.AddScoped<IValidator<LoginUserCommand>, LoginUserValidator> ();
+
+builder.Services.AddSingleton<IEmailSender, SmtpEmailSender> ();
+
+
+builder.Services.AddDataProtection();
+builder.Services.AddIdentityCore<UserData>(options => {
+        options.Password.RequireDigit = true;
+        options.Password.RequiredLength = 6;
+        options.User.RequireUniqueEmail = true;
+    })
+   .AddRoles<IdentityRole<Guid>>()
+   .AddEntityFrameworkStores<AuthServiceDbContext>()
+   .AddSignInManager<SignInManager<UserData>>()
+   .AddDefaultTokenProviders();
+
+
+builder.UseCustomServices ();
+builder.UseCustomSerilog ();
+builder.UseMail ();
 builder.Services.AddEndpointsApiExplorer ();
-builder.Services.AddSwaggerGen (options => {
-    options.SwaggerDoc ("v1", new OpenApiInfo {
-        Title = "Product API",
-        Version = "v1",
-        Description = "������ ������������ Swagger ��� AuthService"
-    });
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("AllowAll", p => p
+       .AllowAnyOrigin()
+       .AllowAnyMethod()
+       .AllowAnyHeader());
 });
 
 var app = builder.Build ();
 
+app.UseCors("AllowAll");
 app.UseSwagger ();
 app.UseSwaggerUI (options => {
-    options.SwaggerEndpoint ("/swagger/v1/swagger.json", "Product API v1");
+    options.SwaggerEndpoint ("/swagger/v1/swagger.json", "AuthService API v1");
 });
 
 app.UseHttpsRedirection ();
 
 app.UseAuthorization ();
-
 app.MapControllers ();
 
 app.Run ();
--- services/AuthService/src/AuthService.Application/Features/RegisterUser/RegisterUserHandler.cs
+++ services/AuthService/src/AuthService.Application/Features/RegisterUser/RegisterUserHandler.cs
@@ -34,7 +34,6 @@ public class RegisterUserHandler : ICommandHandler<Guid, RegisterUserCommand> {
     var createResult = await _userDataRepository.CreateAsync (UserData.Create (dataCommand.Email), dataCommand.Password);
 
 
-
     if (!createResult.IsSuccess) {
       return createResult;
     }
