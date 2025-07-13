99a9dc06839f16f90a43f1eba0592144ac2aba81
artur <105075817+123kapefa@users.noreply.github.com>
2025-07-13 17:04:59 +0300

    feat(answer): Создание сущностей их настройка, а так же DbContext для сервиса AnswerService #answer-2

---
+++ services/QuestionService/src/QuestionService.Infrastructure/AnswerServiceDbContext.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+using QuestionService.Domain.Entities;
+
+namespace QuestionService.Infrastructure;
+
+public class AnswerServiceDbContext : DbContext {
+
+  private readonly string _connectionString;
+  
+  DbSet<Answer> Answers { get; set; }
+  DbSet<AnswerChangingHistory>  AnswerChangingHistories { get; set; }
+
+  public AnswerServiceDbContext (string connectionString) {
+    _connectionString = connectionString;
+  }
+
+  protected override void OnConfiguring (DbContextOptionsBuilder options) {
+    options.UseNpgsql (_connectionString);
+    options.EnableSensitiveDataLogging();
+    options.UseLoggerFactory (CreateLoggerFactory());
+  }
+
+  protected override void OnModelCreating (ModelBuilder builder) {
+    base.OnModelCreating(builder);
+    
+    builder.ApplyConfigurationsFromAssembly (typeof(AnswerServiceDbContext).Assembly);
+  }
+
+  private ILoggerFactory CreateLoggerFactory () =>
+    LoggerFactory.Create(builder => { builder.AddConsole(); });
+
+}
