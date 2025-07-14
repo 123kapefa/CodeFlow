b35f5ca6ddd570236cffa1658b28c008baeecd66
Alexander <121170207+Hissul@users.noreply.github.com>
2025-07-14 12:44:50 +0300

    feat(comment): Добавление Entities, настройка CommentDbContext. Первая миграция.

--- services/CommentService/src/CommentService.Api/Program.cs
+++ services/CommentService/src/CommentService.Api/Program.cs
@@ -1,6 +1,42 @@
-var builder = WebApplication.CreateBuilder(args);
+using CommentService.Infrastucture.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options => {
+    options.SwaggerDoc("v1", new OpenApiInfo {
+        Title = "Product API",
+        Version = "v1",
+        Description = "Пример документации Swagger для QuestionService"
+    });
+
+    options.EnableAnnotations();
+
+});
+
+builder.Services.AddDbContext<CommentDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Main")));
 
 
-var app = builder.Build();
+
+
+WebApplication app = builder.Build();
+
+
+
+app.UseSwagger();
+app.UseSwaggerUI(options => {
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Product API v1");
+});
+
+
+app.MapControllers();
+
+
 
 app.Run();
