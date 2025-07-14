bbbb195e9b464a34567bf193f18e2d521b418edc
Alexander <121170207+Hissul@users.noreply.github.com>
2025-07-14 12:45:33 +0300

    feat(comment): Добавление Entities, настройка CommentDbContext. Первая миграция. (#24)

---
+++ services/CommentService/src/CommentService.Domain/Entities/Comment.cs
@@ -0,0 +1,18 @@
+﻿using CommentService.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommentService.Domain.Entities;
+
+public class Comment {    
+    public Guid Id { get; set; }    
+    public Guid AuthorId { get; set; }
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    
+    public TypeTarget Type { get; set; }
+    public Guid TargetId { get; set; }     
+}
---
+++ services/CommentService/src/CommentService.Domain/Enums/TypeTarget.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommentService.Domain.Enums;
+
+public enum TypeTarget {
+    Question = 1,
+    Answer = 2
+}
