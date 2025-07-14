c01940852d9661bdffa64eb79ce25ba982350301
Alexander <121170207+Hissul@users.noreply.github.com>
2025-07-14 22:25:06 +0300

    feat(comment): Реализован ICommentRepository. Частично реализована его имплементация + Features + ApiEndpoints

---
+++ services/CommentService/src/CommentService.Application/Features/DeleteCommentById/DeleteCommentByIdHandler.cs
@@ -0,0 +1,26 @@
+﻿using Ardalis.Result;
+using CommentService.Domain.Repositories;
+using Contracts.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommentService.Application.Features.DeleteCommentById;
+
+public class DeleteCommentByIdHandler : ICommandHandler<DeleteCommentByIdCommand> {
+
+    private readonly ICommentRepository _commentRepository;
+
+    public DeleteCommentByIdHandler( ICommentRepository commentRepository ) {
+        _commentRepository = commentRepository;
+    }
+
+    public async Task<Result> Handle( DeleteCommentByIdCommand command, CancellationToken token ) {
+
+        Result result = await _commentRepository.DeleteCommentByIdAsync(command.CommentId, token);
+
+        return result.IsSuccess ? Result.Success() : Result.Error(new ErrorList(result.Errors));
+    }
+}
---
+++ services/CommentService/src/CommentService.Domain/Repositories/ICommentRepository.cs
@@ -0,0 +1,23 @@
+﻿using Ardalis.Result;
+using CommentService.Domain.Entities;
+using CommentService.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommentService.Domain.Repositories;
+
+public interface ICommentRepository {
+    Task<Result<IEnumerable<Comment>>> GetQuestionCommentsAsync(
+        TypeTarget target, Guid questionId, CancellationToken token);
+    Task<Result<IEnumerable<Comment>>> GetAnswerCommentsAsync( 
+        TypeTarget target, Guid answerId, CancellationToken token );
+    Task<Result<Comment>> GetCommentByIdAsync( Guid commentId, CancellationToken token );
+
+    Task<Result> CreateCommentAsync(Comment comment, CancellationToken token);
+    Task<Result> UpdateCommentAsync( Comment comment, CancellationToken token );
+
+    Task<Result> DeleteCommentByIdAsync( Guid commentId, CancellationToken token );
+}
