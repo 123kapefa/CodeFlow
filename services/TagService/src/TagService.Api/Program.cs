16da2fd025282f0dfb7fed89d1cddcdfdbac6ede
Alexander <121170207+Hissul@users.noreply.github.com>
2025-07-16 17:34:16 +0300

    feat(tag): Добавление зависимостей для TagService #tag-2 (#30)

--- services/TagService/src/TagService.Api/Program.cs
+++ services/TagService/src/TagService.Api/Program.cs
@@ -1,8 +1,37 @@
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers()
+     .AddJsonOptions(o =>
+           o.JsonSerializerOptions.Converters //конвертер для «enum-как-строка».
+             .Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase)));
+
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options => {
+    options.SwaggerDoc("v1", new OpenApiInfo {
+        Title = "Product API",
+        Version = "v1",
+        Description = "Пример документации Swagger для TagService"
+    });
+
+    options.EnableAnnotations();
+});
 
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(options => {
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Product API v1");
+});
+app.UseDeveloperExceptionPage();
+
+app.MapControllers();
 
 
 
