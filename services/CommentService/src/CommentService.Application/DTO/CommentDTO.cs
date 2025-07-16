3b1a5a4ea610d3f73ef52d99be8e863449bc19f9
Alexander <121170207+Hissul@users.noreply.github.com>
2025-07-16 16:30:33 +0300

    Features/comment/comment 3 repository (#25)
    * feat(comment): Реализован ICommentRepository. Частично реализована его имплементация + Features + ApiEndpoints
    
    * feat(comment): Реализованы CommentRepository, Features и ApiEndpoints
    
    * feat(comment): Исправление замечаний из PR

--- services/CommentService/src/CommentService.Application/DTO/CommentDTO.cs
+++ services/CommentService/src/CommentService.Application/DTO/CommentDTO.cs
@@ -1,13 +1,13 @@
 ﻿using CommentService.Domain.Enums;
 
-namespace CommentService.Domain.Entities;
+namespace CommentService.Application.DTO;
 
-public class Comment {    
-    public Guid Id { get; set; }    
+public class CommentDTO {
+    public Guid Id { get; set; }
     public Guid AuthorId { get; set; }
     public string Content { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    
+
     public TypeTarget Type { get; set; }
-    public Guid TargetId { get; set; }     
+    public Guid TargetId { get; set; }
 }
--- services/CommentService/src/CommentService.Domain/Enums/TypeTarget.cs
+++ services/CommentService/src/CommentService.Domain/Enums/TypeTarget.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CommentService.Domain.Enums;
+﻿namespace CommentService.Domain.Enums;
 
 public enum TypeTarget {
     Question = 1,
