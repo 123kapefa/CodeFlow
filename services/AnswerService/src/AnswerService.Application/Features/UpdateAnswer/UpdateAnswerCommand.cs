fec831a2d7f470669c7fcc3dc757c9d74e2eaac8
artur <105075817+123kapefa@users.noreply.github.com>
2025-07-17 13:25:32 +0300

    ref(answer): Редактирование названий #answer-4

--- services/AnswerService/src/AnswerService.Application/Features/UpdateAnswer/UpdateAnswerCommand.cs
+++
@@ -1,7 +0,0 @@
-using AnswerService.Application.Requests;
-
-using Contracts.Commands;
-
-namespace AnswerService.Application.Features.UpdateAnswer;
-
-public record UpdateAnswerCommand (Guid Id, UpdateAnswerRequest Request) : ICommand;
