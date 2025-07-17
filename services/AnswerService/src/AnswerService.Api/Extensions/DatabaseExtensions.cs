042c32bc3ce4e9ea20dd42dfcc4ce5ec0e46c041
artur <105075817+123kapefa@users.noreply.github.com>
2025-07-17 13:24:54 +0300

    feat(answer): Создание расширений для настройки AnswerService.Api и их подключение #answer-4

---
+++ services/AnswerService/src/AnswerService.Api/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,14 @@
+using AnswerService.Infrastructure;
+
+namespace AnswerService.Api.Extensions;
+
+public static class DatabaseExtensions {
+
+  public static WebApplicationBuilder UseDatabase (this WebApplicationBuilder builder) {
+    builder.Services.AddScoped<AnswerServiceDbContext> (_ =>
+      new AnswerServiceDbContext (builder.Configuration.GetConnectionString (nameof(AnswerServiceDbContext))!));
+
+    return builder;
+  }
+
+}
