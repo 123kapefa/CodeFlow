3eb58ddf10bd30b0a25535e712bbf03be18ac147
artur <105075817+123kapefa@users.noreply.github.com>
2025-07-21 12:08:43 +0300

    feat(shared): Перенос из сервиса QuestionService папку dtos в слой shared для общей видимости #shared-1

--- shared/src/Contracts/QuestionService/DTOs/UpdateQuestionDTO.cs
+++ shared/src/Contracts/QuestionService/DTOs/UpdateQuestionDTO.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace QuestionService.Application.DTO;
+﻿namespace Contracts.QuestionService.DTOs;
 
 public class UpdateQuestionDTO {
     public Guid Id { get; set; }
