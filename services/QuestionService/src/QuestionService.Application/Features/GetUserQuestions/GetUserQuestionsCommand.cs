4510428ea9ab33851f6c70c32cc469c23daebde0
artur <105075817+123kapefa@users.noreply.github.com>
2025-07-21 12:09:01 +0300

    ref(shared): Рефакторинг пространства имен в сервисе QuestionService #shared-1

--- services/QuestionService/src/QuestionService.Application/Features/GetUserQuestions/GetUserQuestionsCommand.cs
+++ services/QuestionService/src/QuestionService.Application/Features/GetUserQuestions/GetUserQuestionsCommand.cs
@@ -1,10 +1,6 @@
-﻿using Contracts.Commands;
-using QuestionService.Domain.Filters;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using QuestionService.Domain.Filters;
+
+using Abstractions.Commands;
 
 namespace QuestionService.Application.Features.GetUserQuestions;
 public record GetUserQuestionsCommand( Guid UserId, PageParams PageParams, SortParams SortParams )  : ICommand;
