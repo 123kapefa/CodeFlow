b0f58a3323a69396bd4d5c6a2090a54e6b0c9f39
artur <105075817+123kapefa@users.noreply.github.com>
2025-07-22 19:28:06 +0300

    ref(UserService): Переделано подключение к бд. Созданы расширения для Program.cs #refactor-api

---
+++ services/UserService/src/UserService.Api/Extensions/HandlerCollectionExtensions.cs
@@ -0,0 +1,40 @@
+using Abstractions.Commands;
+
+using Ardalis.Result;
+
+using Contracts.UserService.DTOs;
+
+using FluentValidation;
+
+using UserService.Application.Features.CreateUserInfo;
+using UserService.Application.Features.DeleteUser;
+using UserService.Application.Features.GetUserFullInfo;
+using UserService.Application.Features.GetUsers;
+using UserService.Application.Features.UpdateUserInfo;
+using UserService.Application.Features.UpdateUserReputation;
+using UserService.Application.Features.UpdateUserVisit;
+using UserService.Domain.Repositories;
+using UserService.Infrastructure.Repositories;
+
+namespace UserService.Api.Extensions;
+
+public static class HandlerCollectionExtensions {
+
+  public static WebApplicationBuilder AddHandlers (this WebApplicationBuilder builder) {
+    
+    builder.Services.AddScoped<IUserInfoRepository, UserInfoRepository>();
+
+    builder.Services.AddScoped<ICommandHandler<UpdateUserInfoCommand>, UpdateUserInfoHandler>();
+    builder.Services.AddScoped<ICommandHandler<PagedResult<IEnumerable<UserShortDTO>>, GetUsersCommand>, GetUsersHandler>();
+    builder.Services.AddScoped<ICommandHandler<DeleteUserCommand>, DeleteUserHandler>();
+    builder.Services.AddScoped<ICommandHandler<UpdateUserReputationCommand>, UpdateUserReputationHandler>();
+    builder.Services.AddScoped<ICommandHandler<UpdateUserVisitCommand>, UpdateUserVisitHandler>();
+    builder.Services.AddScoped<ICommandHandler<Result<UserFullInfoDTO>, GetUserFullInfoCommand>, GetUserFullInfoHandler>();
+    builder.Services.AddScoped<ICommandHandler<CreateUserInfoCommand>, CreateUserInfoHandler>();
+
+    builder.Services.AddScoped<IValidator<UpdateUserInfoCommand>, UpdateUserInfoValidator>();
+    
+    return builder;
+  }
+
+}
