65b2d973398df08ed98ca520a4a8cdb3f0c57cd4
artur <105075817+123kapefa@users.noreply.github.com>
2025-07-22 20:22:34 +0300

    ref(AuthService): Переделано подключение к бд. Созданы расширения для Program.cs #refactor-api

--- services/AuthService/src/AuthService.Api/Extensions/BaseExtensions.cs
+++ services/AuthService/src/AuthService.Api/Extensions/BaseExtensions.cs
@@ -4,20 +4,31 @@ namespace AuthService.Api.Extensions;
 
 public static class BaseExtensions {
 
-  public static WebApplicationBuilder UseBase (this WebApplicationBuilder builder) {
+  public static WebApplicationBuilder AddBase (this WebApplicationBuilder builder) {
+    
+    var config = new ConfigurationBuilder()
+     .AddUserSecrets<Program>()
+     .Build();
+    
     builder.Services.AddControllers ();
     builder.Services.AddEndpointsApiExplorer ();
     builder.Services.AddOpenApi ();
-    
-    builder.Services.AddSwaggerGen (options => {
-      options.SwaggerDoc ("v1", new OpenApiInfo {
-        Title = "Product API",
-        Version = "v1",
-        Description = "Swagger AuthService"
-      });
-    });
 
+    builder.Services.AddCors(opt => {
+      opt.AddPolicy("AllowAll", p => p
+       .AllowAnyOrigin()
+       .AllowAnyMethod()
+       .AllowAnyHeader());
+    });
+    
     return builder;
+  }
+  
+  public static IApplicationBuilder UseBase (this IApplicationBuilder app) {
+    
+    app.UseDeveloperExceptionPage ();
+    app.UseCors("AllowAll");
+    return app;
   }
 
 }
