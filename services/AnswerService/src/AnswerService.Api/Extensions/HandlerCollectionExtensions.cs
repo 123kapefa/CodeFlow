503aad13caaa4caf64cf12138cb439a8408c07d1
artur <105075817+123kapefa@users.noreply.github.com>
2025-07-28 12:15:20 +0300

    feat(rabbit): Реализовано взаимодействие между AnswerService и UserService при лайке и дизлайке ответа, где в UserService обновляется репутация пользователя RabbitMQ #rabbitmq-1

--- services/AnswerService/src/AnswerService.Api/Extensions/HandlerCollectionExtensions.cs
+++ services/AnswerService/src/AnswerService.Api/Extensions/HandlerCollectionExtensions.cs
@@ -1,4 +1,3 @@
-using AnswerService.Application.Features.AcceptAnswer;
 using AnswerService.Application.Features.CreateAnswer;
 using AnswerService.Application.Features.DeleteAnswer;
 using AnswerService.Application.Features.EditAnswer;
@@ -7,6 +6,7 @@ using AnswerService.Application.Features.GetAnswersByUserId;
 
 using Abstractions.Commands;
 
+using AnswerService.Application.Features.UpdateAnswerAccept;
 using AnswerService.Domain.Repositories;
 using AnswerService.Infrastructure.Repositories;
 
@@ -26,7 +26,7 @@ public static class HandlerCollectionExtensions {
     builder.Services.AddScoped<ICommandHandler<CreateAnswerResponse, CreateAnswerCommand>, CreateAnswerHandler> ();
     builder.Services.AddScoped<ICommandHandler<EditAnswerCommand>, EditAnswerHandler> ();
     builder.Services.AddScoped<ICommandHandler<DeleteAnswerCommand>, DeleteAnswerHandler> ();
-    builder.Services.AddScoped<ICommandHandler<AcceptAnswerCommand>, AcceptAnswerHandler> ();
+    builder.Services.AddScoped<ICommandHandler<UpdateAnswerAcceptCommand>, UpdateAnswerAcceptHandler> ();
     builder.Services.AddScoped<ICommandHandler<GetAnswersResponse, GetAnswersByUserIdCommand>, GetAnswersByUserIdHandler> ();
     builder.Services.AddScoped<ICommandHandler<GetAnswersResponse, GetAnswersByQuestionIdCommand>, GetAnswersByQuestionIdHandler> ();
     
