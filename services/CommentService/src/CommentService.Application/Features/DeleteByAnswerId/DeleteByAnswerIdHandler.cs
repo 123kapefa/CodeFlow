b791a978485f0be864daf00e87fcbd0ea0189a01
Alexander <121170207+Hissul@users.noreply.github.com>
2025-07-29 21:56:06 +0300

    Features/comment/comment 4 adding functionality (#55)
    * ref(auth): Реализовано взаимодействие между AuthService и UserService при регистрации через RabbitMQ #auth-7
    
    * feat(rabbit): В AuthService бобавлен publicher при логине пользователя. В UserService длбавлен comsumer при логине пользователя. Для AnswerService длбавлен метод расшинерения MassTransitExtensions и в AnswerServiceDbContext длбалены новые таблицы lkz RabbitMQ outbox(без миграции)
    
    * feat(rabbit): Реализовано взаимодействие между AnswerService и QuestionService при создании нового ответа, где в QuestionService обновляется счетчик количество ответов вопроса через RabbitMQ #rabbitmq-1
    
    * feat(rabbit): Реализовано взаимодействие между AnswerService и UserService при лайке и дизлайке ответа, где в UserService обновляется репутация пользователя RabbitMQ #rabbitmq-1
    
    * feat(comment): Реализовано удаление все коментариев пользователя #comment-4
    
    * feat(Comment): Реализован метод удаления коментариев для ответа
    
    * Слить pull request #54
    
    * ref(auth): Реализовано взаимодействие между AuthService и UserService…
    
    * feat(rabbit): В AuthService бобавлен publicher при логине пользовател…
    
    * feat(question): Реализован метод уменьшения AnswerCount в Questions п…
    
    * ref(auth): Реализовано взаимодействие между AuthService и UserService…
    
    * feat(rabbit): В AuthService бобавлен publicher при логине пользовател…
    
    * feat(rabbit): Реализовано взаимодействие между AnswerService и Questi…
    
    * feat(rabbit): Реализовано взаимодействие между AnswerService и UserSe…
    
    * feat(question): Реализован метод уменьшения AnswerCount в Questions п…
    
    * Merge remote-tracking branch 'origin/features/question/question-6-add…
    
    * ref(auth): Реализовано взаимодействие между AuthService и UserService при регистрации через RabbitMQ #auth-7
    
    * feat(rabbit): В AuthService бобавлен publicher при логине пользователя. В UserService длбавлен comsumer при логине пользователя. Для AnswerService длбавлен метод расшинерения MassTransitExtensions и в AnswerServiceDbContext длбалены новые таблицы lkz RabbitMQ outbox(без миграции)
    
    * feat(rabbit): Реализовано взаимодействие между AnswerService и QuestionService при создании нового ответа, где в QuestionService обновляется счетчик количество ответов вопроса через RabbitMQ #rabbitmq-1
    
    * feat(rabbit): Реализовано взаимодействие между AnswerService и UserService при лайке и дизлайке ответа, где в UserService обновляется репутация пользователя RabbitMQ #rabbitmq-1
    
    ---------
    
    Co-authored-by: artur <105075817+123kapefa@users.noreply.github.com>

---
+++ services/CommentService/src/CommentService.Application/Features/DeleteByAnswerId/DeleteByAnswerIdHandler.cs
@@ -0,0 +1,31 @@
+﻿using Abstractions.Commands;
+using Ardalis.Result;
+using CommentService.Domain.Entities;
+using CommentService.Domain.Enums;
+using CommentService.Domain.Repositories;
+
+namespace CommentService.Application.Features.DeleteByAnswerId;
+
+public class DeleteByAnswerIdHandler : ICommandHandler<DeleteByAnswerIdCommand> {
+
+    private readonly ICommentRepository _commentRepository;
+
+    public DeleteByAnswerIdHandler( ICommentRepository commentRepository ) {
+        _commentRepository = commentRepository;
+    }
+
+    public async Task<Result> Handle( DeleteByAnswerIdCommand command, CancellationToken token ) {
+
+        if(command.AnswerId == Guid.Empty)
+            return Result.Error("Answer ID cannot be empty.");
+
+        Result<IEnumerable<Comment>> resultComments = 
+            await _commentRepository.GetCommentsAsync(TypeTarget.Answer, command.AnswerId, token);
+        if(!resultComments.IsSuccess)
+            return Result.Error(new ErrorList(resultComments.Errors));
+
+        Result result = await _commentRepository.DeleteAnswerCommentsAsync(resultComments.Value, token);
+
+        return result.IsSuccess ? Result.Success() : Result.Error(new ErrorList(result.Errors));
+    }
+}
