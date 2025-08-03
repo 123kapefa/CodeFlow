0891b72217d287e57b50c04ecc164e615d391178
Alexander <121170207+Hissul@users.noreply.github.com>
2025-08-03 22:12:27 +0300

    feat(web): Создание структуры проекта. Верстка статической компонентов.(header, footer, sidebar)

--- web/codeflow-web/src/App.js
+++ web/codeflow-web/src/App.js
@@ -1,11 +1,35 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
+import Footer from './components/Footer/Footer';
+
+import Home from './pages/Home';
+import Questions from './pages/Questions';
+import Tags from './pages/Tags';
+import Users from './pages/Users';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Проэкт</h1>
-      </header>
+      <div className="container-xxl">
+        <Header />
+
+        <div className="main">
+          <Sidebar />
+          <main className="content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/questions" element={<Questions />} />
+              <Route path="/tags" element={<Tags />} />
+              <Route path="/users" element={<Users />} />
+            </Routes>
+          </main>
+        </div>
+      </div>
+
+      <Footer />
     </div>
   );
 }
