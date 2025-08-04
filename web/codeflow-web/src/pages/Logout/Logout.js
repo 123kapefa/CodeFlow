b9d5314eec6185f378033da748723712761dedc7
Alexander <121170207+Hissul@users.noreply.github.com>
2025-08-04 20:01:19 +0300

    feat(web): Исправление ошибок

--- web/codeflow-web/src/pages/Logout/Logout.js
+++ web/codeflow-web/src/pages/Logout/Logout.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
-import { RefreshToken } from "../authClient";
+import { RefreshToken } from "../../features/RefreshToken/RefreshToken";
 
 function Logout() {
   const navigate = useNavigate();
