66fc0d5c370cbcb89b8f8a08f3c8a86ba42c2c84
Alexander <121170207+Hissul@users.noreply.github.com>
2025-08-04 14:39:27 +0300

    feat(web): Реализованы страницы Login, Singup и Logout. В AuthService добавлен CORS.

--- services/ApiGateway/src/ApiGateway.Api/Program.cs
+++ services/ApiGateway/src/ApiGateway.Api/Program.cs
@@ -7,13 +7,28 @@ var builder = WebApplication.CreateBuilder (args);
 builder.AddConfig ();
 builder.AddAuth ();
 
+// --- CORS ---
+builder.Services.AddCors(options => {
+    options.AddPolicy("ReactDev", policy =>
+        policy.WithOrigins(
+                "http://localhost:3000",
+                "http://127.0.0.1:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials()); // если шлёшь cookies/авторизацию
+});
+
 var app = builder.Build();
 
 app.UseRouting();
 
+// CorsMiddleware должен стоять между UseRouting и UseAuth
+app.UseCors("ReactDev");
+
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapReverseProxy ();
+// Применяем CORS к endpoint'у прокси (важно!)
+app.MapReverseProxy().RequireCors("ReactDev");
 
 app.Run();
