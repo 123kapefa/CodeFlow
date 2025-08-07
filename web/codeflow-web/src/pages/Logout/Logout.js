df5662742e88de48d1073d5dc33dd195b844c10d
Alexander <121170207+Hissul@users.noreply.github.com>
2025-08-07 11:03:22 +0300

    Features/web/web 2 create tags page (#65)
    * feat(web): Верстка страницы Tags. Добавлена возможность отслеживать тэг.
    
    * feat(web): Исправлена логика Login, Logout и Registration - теперь через AuthProvider. Верстка UserProfile
    
    * feat(web): Добавлен хук useAuthFetch, добавляющий jwt в заголовок запроса. Создана HomePage

--- web/codeflow-web/src/pages/Logout/Logout.js
+++ web/codeflow-web/src/pages/Logout/Logout.js
@@ -1,71 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import Cookies from "js-cookie";
-import { RefreshToken } from "../../features/RefreshToken/RefreshToken";
+import { useAuth } from "../../features/Auth/AuthProvider "; 
 
 function Logout() {
   const navigate = useNavigate();
-  const [error, setError] = useState("");
+  const { logout } = useAuth();
 
   useEffect(() => {
-    const logout = async () => {
-      try {
-        const refreshToken = Cookies.get("refresh_token");
-        const token = Cookies.get("jwt");
+    (async () => {
+      await logout();
+      navigate("/login", { replace: true });
+    })();
+  }, [logout, navigate]);
 
-        if (!refreshToken) {
-          navigate("/login", { replace: true });
-          return;
-        }
-
-        const headers = { "Content-Type": "application/json" };
-        if (token) headers.Authorization = `Bearer ${token}`;
-
-        const response = await fetch("http://localhost:5000/api/auth/logout", {
-          method: "POST",
-          headers,
-          body: JSON.stringify( refreshToken ),
-        });
-
-        if (response.status === 401) {
-          const refresh_response = RefreshToken();
-
-          if (refresh_response) {
-            response = await fetch("http://localhost:5000/api/auth/logout", {
-              method: "POST",
-              headers,
-              body: JSON.stringify( refreshToken ),
-            });
-          }
-        }
-
-        if (!response.ok) {
-          const err = await response.json();   
-          throw new Error(err.message || "Ошибка выхода");
-        }
-      } 
-      catch (err) {
-        setError(err.message);
-      } 
-      finally {
-        Cookies.remove("jwt", { path: "/" });
-        Cookies.remove("refresh_token", { path: "/" });
-        navigate("/login", { replace: true });
-      }
-    };
-
-    logout();
-  }, [navigate]);
-
-  return (
-    <div className="d-flex justify-content-center align-items-center vh-100">
-      {error ? (
-        <div className="alert alert-danger">{error}</div>
-      ) : (
-        <div>Logging out...</div>
-      )}
-    </div>
-  );
+  return <div>Logging out…</div>;
 }
 
 export default Logout;
