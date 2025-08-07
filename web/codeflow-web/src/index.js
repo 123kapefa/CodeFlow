abc91b7a6d67abf44885c994e6ab92c63789d239
Alexander <121170207+Hissul@users.noreply.github.com>
2025-08-07 11:03:22 +0300

    Features/web/web 2 create tags page (#65)
    * feat(web): Верстка страницы Tags. Добавлена возможность отслеживать тэг.
    
    * feat(web): Исправлена логика Login, Logout и Registration - теперь через AuthProvider. Верстка UserProfile
    
    * feat(web): Добавлен хук useAuthFetch, добавляющий jwt в заголовок запроса. Создана HomePage

--- web/codeflow-web/src/index.js
+++ web/codeflow-web/src/index.js
@@ -7,7 +7,7 @@ import App from "./App";
 import "./index.css";
 import { AuthProvider } from "./features/Auth/AuthProvider ";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
